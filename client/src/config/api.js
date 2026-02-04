e4fe81447eda57464d73a7b685b259b4add67319
Gaf Ohad <gafohad883@gmail.com>
2026-02-04 02:57:53 +0200

    Fix API url for production

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,6 +1,6 @@
 // src/config/api.js
 // כרגע אנחנו עובדים מקומית.
 // כשנעלה לענן (Render), נשנה את השורה הזאת לכתובת האמיתית.
-const API_BASE_URL = "https://tired-cups-own.loca.lt/api";
+const API_BASE_URL = "/api";
 
 export default API_BASE_URL;
